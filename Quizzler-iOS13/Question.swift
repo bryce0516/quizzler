0445c90c8e952b5f669fe6b97e13656cb1518ef7
bryce0516 <jayhspark@gmail.com>
2022-04-06 22:16:38 +0900

    update

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Hye Sung Park on 2022/04/06.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q:String, a:String) {
+        text = q
+        answer = a
+    }
+}
